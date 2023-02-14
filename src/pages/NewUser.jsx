777ba6946500c52be4c211e7fb5124fcad634b45
mythily-muthu <mythilimuthu18@gmail.com>
2023-02-14 17:42:00 +0530

    Feat: added new user page and edit page

--- src/pages/NewUser.jsx
+++ src/pages/NewUser.jsx
@@ -1,81 +1,163 @@
+import axios from "axios";
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const NewUser = () => {
   let navigate = useNavigate();
 
-  let [users, setUsers] = useState({
+  let [userState, setUserState] = useState({
     name: "",
     gender: "",
     email: "@gmail.com",
+    status: "",
   });
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     console.log("submitted");
+    await axios.post("url", userState);
   };
 
   //handle back functin
   const handleBack = (e) => {
     e.preventDefault();
-    console.log("done");
+    console.log("welcome to home page");
     navigate("/");
   };
 
   return (
     <div>
-      <div className="bg-gray-300 h-screen w-full ">
+      <div className="h-full w-full flex flex-col  ">
         <form className="container" onSubmit={handleSubmit}>
-          <div className="card">
-            <div className="my-5 text-2xl font-medium flex justify-center">
-              <h2 className="p-10">Create new user</h2>
+          <div className="flex fixed left-0 p-2 items-center">
+            <button
+              className="px-3 py-1 bg-red-500 text-white rounded cursor-pointer "
+              onClick={handleBack}
+            >
+              Back
+            </button>
+          </div>
+          <div className="h-full w-screen justify-center">
+            <div className="py-10 text-2xl flex justify-center">
+              <h3 className="text-3xl">Create new user</h3>
             </div>
-            <div className="flex flex-col items-center w-full">
-              <div className="flex flex-col items-center p-10 w-full ">
-                <div className=" flex flex-col  ">
-                  <label className="p-5 mx-5">Name</label>
-                  <input
-                    className="w-[300px] rounded mx-10 p-2"
-                    required
-                    placeholder="Name"
-                    type="text"
-                  ></input>
-                </div>
 
-                <div className="flex flex-col">
-                  <label className="p-5 mx-5">Gender</label>
-                  <input
-                    className="w-[300px] rounded mx-10 p-2"
-                    required
-                    placeholder="Gender"
-                    type="text"
-                  ></input>
-                </div>
+            <div className="flex flex-col items-center justify-center  w-screen h-full ">
+              <div className=" flex flex-col ">
+                <label className=" py-5  text-gray-500">Name</label>
+                <input
+                  className=" rounded px-10 py-2 border border-gray-400 outline-none"
+                  required
+                  placeholder="Name"
+                  type="text"
+                  value={userState.name}
+                  onChange={(e) => {
+                    setUserState({ ...setUserState, name: e.target.value });
+                  }}
+                ></input>
+              </div>
 
-                <div className="flex flex-col">
-                  <label className="p-5 mx-5">Email</label>
-                  <input
-                    className="w-[300px] rounded mx-10 p-2"
-                    required
-                    placeholder="Email"
-                    type="text"
-                  ></input>
-                </div>
+              <div className="flex flex-col">
+                <label className="py-5 text-gray-500">Email</label>
+                <input
+                  className=" rounded px-10 py-2 border border-gray-400 outline-none"
+                  required
+                  placeholder="Email"
+                  type="text"
+                  value={userState.email}
+                  onChange={(e) => {
+                    setUserState({ ...setUserState, email: e.target.value });
+                  }}
+                ></input>
+              </div>
 
-                <div className="mt-10 flex gap-5">
-                  <button
-                    className="p-2 bg-slate-900 text-white rounded"
-                    type="submit"
-                  >
-                    Save
-                  </button>
-                  <button
-                    className="p-2 bg-slate-900 text-white rounded"
-                    onClick={handleBack}
-                  >
-                    Back
-                  </button>
+              <div className="flex flex-col">
+                <label className="py-5 text-gray-500">Gender</label>
+                <input
+                  className=" rounded px-10 py-2 border border-gray-400 outline-none"
+                  required
+                  placeholder="gender"
+                  type="text"
+                  value={userState.gender}
+                  onChange={(e) => {
+                    setUserState({ ...setUserState, gender: e.target.value });
+                  }}
+                ></input>
+              </div>
+
+              {/* <div className="flex py-5 ml-20 ">
+                <div className="flex ml-5 ">
+                  <label className="py-5 text-gray-500 ">Gender</label>
                 </div>
+                <div className="flex ml-10 gap-3">
+                  <input
+                    className="  "
+                    type="radio"
+                    name="gender"
+                    value="male"
+                    onChange={(e) => {
+                      setUserState(e.target.value);
+                    }}
+                  />
+                  male
+                  <input
+                    className=""
+                    type="radio"
+                    name="gender"
+                    value="female"
+                    onChange={(e) => {
+                      setUserState(e.target.value);
+                    }}
+                  />
+                  female
+                  <input
+                    className=" "
+                    type="radio"
+                    name="gender"
+                    value="others"
+                    onChange={(e) => {
+                      setUserState(e.target.value);
+                    }}
+                  />
+                  others
+                </div>
+              </div>
+
+              */}
+
+              <div className="flex mr-16 py-10 ">
+                <label className=" mr-14 text-gray-500">Status</label>
+                <div className="flex gap-1 ">
+                  <input
+                    type="radio"
+                    name="status"
+                    value="active"
+                    onChange={(e) => {
+                      setUserState({ ...setUserState, status: e.target.value });
+                    }}
+                  />
+                  active
+                </div>
+                <div className=" flex gap-1">
+                  <input
+                    type="radio"
+                    name="status"
+                    value="inactive"
+                    onChange={(e) => {
+                      setUserState({ ...setUserState, status: e.target.value });
+                    }}
+                  />
+                  inactive
+                </div>
+              </div>
+
+              <div className="py-1">
+                <button
+                  className=" py-1 px-10 w-full flex bg-green-600 text-white rounded cursor-pointer items-center"
+                  type="submit"
+                >
+                  Save
+                </button>
               </div>
             </div>
           </div>
--- src/pages/UpdateUser.jsx
+++ src/pages/UpdateUser.jsx
@@ -1,7 +1,130 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const UpdateUser = () => {
-  return <div>UpdateUser</div>;
+  let navigate = useNavigate();
+
+  let [userState, setUserState] = useState({
+    name: "",
+    gender: "",
+    email: "@gmail.com",
+    status: "",
+  });
+
+  const handleUpdate = async (e) => {
+    e.preventDefault();
+    console.log("updated your user data");
+    await axios.post("url", userState);
+  };
+
+  //handle back functin
+  const handleBack = (e) => {
+    e.preventDefault();
+    console.log("back to home page");
+    navigate("/");
+  };
+
+  return (
+    <div>
+      <div className="h-full w-full flex flex-col  ">
+        <form className="container" onSubmit={handleUpdate}>
+          <div className="flex fixed left-0 p-2 items-center">
+            <button
+              className="px-3 py-1 bg-red-500 text-white rounded cursor-pointer "
+              onClick={handleBack}
+            >
+              Back
+            </button>
+          </div>
+          <div className="h-full w-screen justify-center">
+            <div className="py-10 text-2xl flex justify-center">
+              <h3 className="text-3xl">Update user</h3>
+            </div>
+
+            <div className="flex flex-col items-center justify-center  w-screen h-full ">
+              <div className=" flex flex-col ">
+                <label className=" py-5  text-gray-500">Name</label>
+                <input
+                  className=" rounded px-10 py-2 border border-gray-400 outline-none"
+                  required
+                  placeholder="Name"
+                  type="text"
+                  value={userState.name}
+                  onChange={(e) => {
+                    setUserState({ ...setUserState, name: e.target.value });
+                  }}
+                ></input>
+              </div>
+
+              <div className="flex flex-col">
+                <label className="py-5 text-gray-500">Email</label>
+                <input
+                  className=" rounded px-10 py-2 border border-gray-400 outline-none"
+                  required
+                  placeholder="Email"
+                  type="text"
+                  value={userState.email}
+                  onChange={(e) => {
+                    setUserState({ ...setUserState, email: e.target.value });
+                  }}
+                ></input>
+              </div>
+
+              <div className="flex flex-col">
+                <label className="py-5 text-gray-500">Gender</label>
+                <input
+                  className=" rounded px-10 py-2 border border-gray-400 outline-none"
+                  required
+                  placeholder="gender"
+                  type="text"
+                  value={userState.gender}
+                  onChange={(e) => {
+                    setUserState({ ...setUserState, gender: e.target.value });
+                  }}
+                ></input>
+              </div>
+
+              <div className="flex mr-16 py-10 ">
+                <label className=" mr-14 text-gray-500">Status</label>
+                <div className="flex gap-1 ">
+                  <input
+                    type="radio"
+                    name="status"
+                    value="active"
+                    onChange={(e) => {
+                      setUserState({ ...setUserState, status: e.target.value });
+                    }}
+                  />
+                  active
+                </div>
+                <div className=" flex gap-1">
+                  <input
+                    type="radio"
+                    name="status"
+                    value="inactive"
+                    onChange={(e) => {
+                      setUserState({ ...setUserState, status: e.target.value });
+                    }}
+                  />
+                  inactive
+                </div>
+              </div>
+
+              <div className="py-1">
+                <button
+                  className=" py-1 px-10 w-full flex bg-green-600 text-white rounded cursor-pointer items-center"
+                  type="submit"
+                >
+                  Update
+                </button>
+              </div>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default UpdateUser;
